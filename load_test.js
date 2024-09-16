60e1bbc28243e96eaa796aafcda838849095b009
ABHIRUP <abhirupsaha02@gmail.com>
2024-09-17 01:50:31 +0530

    trying husky

--- load_test.js
+++ load_test.js
@@ -5,7 +5,7 @@ const URL = "http://localhost:80"
 
 export let options = {
     duration: "20s",
-    vus: 18,
+    vus: 12,
     thresholds: {
         http_req_failed: ['rate<0.0001'], // http errors should be less than 0.01%
         http_req_duration: ['p(99)<50'] // 99% of requests should be below 50ms
