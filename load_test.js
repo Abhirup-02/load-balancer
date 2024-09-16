272b6d630cc22d2890d4cdedda0d85f1eea87c0a
ABHIRUP <abhirupsaha02@gmail.com>
2024-09-16 22:06:29 +0530

    req duration threshold

--- load_test.js
+++ load_test.js
@@ -7,7 +7,8 @@ export let options = {
     duration: "20s",
     vus: 18,
     thresholds: {
-        http_req_failed: ['rate<0.0001'] // http errors should be less than 0.01%
+        http_req_failed: ['rate<0.0001'], // http errors should be less than 0.01%
+        http_req_duration: ['p(99)<50'] // 99% of requests should be below 50ms
     }
 }
 
@@ -16,7 +17,7 @@ export default function () {
 
     if (getResponse.status == 200) {
         check(getResponse, {
-            "response time is less than 50ms": (res) => res.timings.duration < 50,
+            "Response time is less than 50ms": (res) => res.timings.duration < 50,
         })
     }
     else {
