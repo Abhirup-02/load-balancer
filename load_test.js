20c83b55dd7cf984c9f37d35a5c23ea35e153ee9
ABHIRUP <abhirupsaha02@gmail.com>
2024-09-16 20:27:41 +0530

    threshold added

--- load_test.js
+++ load_test.js
@@ -6,6 +6,9 @@ const URL = "http://localhost:80"
 export let options = {
     duration: "20s",
     vus: 18,
+    thresholds: {
+        http_req_failed: ['rate<0.0001'] // http errors should be less than 0.01%
+    }
 }
 
 export default function () {
